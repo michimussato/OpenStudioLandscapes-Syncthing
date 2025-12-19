58ffd24f8451dc8703ff30edd793a075ff3411c0
Michael Mussato <michimussato@gmail.com>
2025-12-19 15:01:34 +0100

    47-refactor-to-config-based-landscapes: OpenStudioLandscapes-OpenCue refactored

--- src/OpenStudioLandscapes/Syncthing/definitions.py
+++ src/OpenStudioLandscapes/Syncthing/definitions.py
@@ -4,20 +4,14 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.Syncthing.assets
-import OpenStudioLandscapes.Syncthing.constants
 
 assets = load_assets_from_modules(
     modules=[OpenStudioLandscapes.Syncthing.assets],
-)
-
-constants = load_assets_from_modules(
-    modules=[OpenStudioLandscapes.Syncthing.constants],
 )
 
 
 defs = Definitions(
     assets=[
         *assets,
-        *constants,
     ],
 )
