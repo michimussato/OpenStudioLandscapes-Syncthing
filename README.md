6d0a485566fe0a5495143f1a192622bad52ef18d
Michael Mussato <michimussato@gmail.com>
2026-01-22 10:39:54 +0100

    - nox

--- README.md
+++ README.md
@@ -69,7 +69,7 @@ A local config store location will be created if it doesn't exist, together with
 > [!TIP]
 > 
 > To specify a config store location different than
-> the default, you can do so be setting the environment variable
+> the default, you can do so by setting the environment variable
 > `OPENSTUDIOLANDSCAPES__CONFIGSTORE_ROOT`:
 > 
 > ```shell
@@ -480,4 +480,4 @@ To follow up on the previous LinkedIn publications, visit:
 
 ***
 
-Last changed: **2025-12-31 12:38:52 UTC**
+Last changed: **2026-01-22 09:27:50 UTC**
--- src/OpenStudioLandscapes/Syncthing/doc/readme.py
+++ src/OpenStudioLandscapes/Syncthing/doc/readme.py
@@ -19,28 +19,22 @@ def readme_feature(
 
     doc.add_paragraph(
         snakemd.Inline(
-            text=textwrap.dedent(
-                """\
+            text=textwrap.dedent("""\
                 Logo Syncthing\
-                """
-            ),
+                """),
             image="https://syncthing.net/img/logo-horizontal.svg",
             link="https://syncthing.net/",
         ).__str__()
     )
 
-    doc.add_paragraph(
-        text=textwrap.dedent(
-            """\
+    doc.add_paragraph(text=textwrap.dedent("""\
             Syncthing is a continuous file synchronization program. 
             It synchronizes files between two or more computers in real 
             time, safely protected from prying eyes. Your data is your 
             data alone and you deserve to choose where it is stored, 
             whether it is shared with some third party, and how it’s 
             transmitted over the internet.\
-            """
-        )
-    )
+            """))
 
     doc.add_heading(
         text="Official Documentation",
