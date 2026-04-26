371ebed3affc2a397c75af2a37d4b37123f818d1
Michael Mussato <michimussato@gmail.com>
2026-04-26 17:52:02 +0200

    add computed_field to properties

--- src/OpenStudioLandscapes/Syncthing/config/models.py
+++ src/OpenStudioLandscapes/Syncthing/config/models.py
@@ -5,6 +5,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
+    computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -111,6 +112,7 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
+    @computed_field
     @property
     def syncthing_config_dir_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
