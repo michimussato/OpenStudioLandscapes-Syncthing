42466311389a7e6ae5165b3065fafe5146edaf3d
Michael Mussato <michimussato@gmail.com>
2026-04-26 21:07:49 +0200

    remove compute_fields. serializing them causes problems and is also not needed.

--- src/OpenStudioLandscapes/Syncthing/config/models.py
+++ src/OpenStudioLandscapes/Syncthing/config/models.py
@@ -5,7 +5,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
-    computed_field,
+    # computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -112,7 +112,7 @@ class Config(FeatureBaseModel):
     )
 
     # EXPANDABLE PATHS
-    @computed_field
+    # @computed_field
     @property
     def syncthing_config_dir_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
