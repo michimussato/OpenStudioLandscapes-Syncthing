0e946748f83561133cc611892659811fdd575e89
Michael Mussato <michimussato@gmail.com>
2026-05-04 14:28:36 +0200

    nox (readmes failed)

--- src/OpenStudioLandscapes/Syncthing/config/models.py
+++ src/OpenStudioLandscapes/Syncthing/config/models.py
@@ -1,14 +1,12 @@
 import pathlib
 from typing import List
 
+from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 from pydantic import (
     Field,
     PositiveInt,
 )
-
-from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
-
-from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
 from OpenStudioLandscapes.Syncthing import constants, dist
 
@@ -133,6 +131,7 @@ if __name__ == "__main__":
     CONFIG_STR = Config.get_docs()
 else:
     import yaml
+
     CONFIG_STR = yaml.dump(
         Config.model_json_schema(mode="serialization"),
     )
